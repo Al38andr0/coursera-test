ba373dc3e74cb92a6772f8714ebd5fe7e0d980b7
Alessandro <alessandro.iacovacci@outlook.com>
2016-09-16 15:54:47 +0100

    Finished V3

--- module2/controllers.js
+++ module2/controllers.js
@@ -3,11 +3,9 @@ ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyShoppingController(ShoppingListCheckOffService) {
     var itemsToBuy = this;
     itemsToBuy.items = ShoppingListCheckOffService.getItemsToBuy();
-    itemsToBuy.emptyList = false;
 
     itemsToBuy.buyItem = function(id) {
         ShoppingListCheckOffService.buyItem(id);
-        itemsToBuy.emptyList = (itemsToBuy.items.length === 0);
     };
 }
 
